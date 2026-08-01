937242e33fd782ede6ae4cbf8ff48b30fc6b155f
Akshay02-cmd <akshaydpatil1646@gmail.com>
2026-08-01 18:05:18 +0530

    feat: update API base URL in .env and remove baseUrl from jsconfig.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+const __dirname = path.resolve()
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
